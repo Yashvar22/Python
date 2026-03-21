da54ba6a891b90e958e50aba9014a4540b3418f0
Yashvar22 <yashvardhan.22scse1012863@galgotiasuniversity.edu.in>
2026-03-21 14:11:54 +0530

    Dict

---
+++ Chapter-11-Dictionary/Practice.py
@@ -0,0 +1,20 @@
+#1->pyhton script to merge two python dict
+
+"""
+d={10:100,20:200,30:300,40:400};
+d2={5:500,6:600}
+
+for i in d2:
+  d[i]=d2[i]
+
+print(d)
+"""
+
+#2->Python program to sum all the values in a dict
+d={10:100,20:200,30:300,40:400};
+
+sum=0
+for i in d:
+   sum=sum+d[i]
+
+print(d)
