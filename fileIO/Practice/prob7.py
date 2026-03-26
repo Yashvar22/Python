9e135ef2af0f059e020ae99867c3060898ce0689
Yashvar22 <yashvardhan.22scse1012863@galgotiasuniversity.edu.in>
2026-03-26 16:51:04 +0530

    File I/O

---
+++ fileIO/Practice/prob7.py
@@ -0,0 +1,13 @@
+
+with open("fileIO/Practice/log.txt") as f:
+      lines=f.readlines();
+
+lineno=1;
+for line in lines:
+      
+    if("python" in line):
+      print(f"yes pyhton is preent in line no:{line}")
+      lineno+=1;
+   
+else:
+    print("No python is not present")
